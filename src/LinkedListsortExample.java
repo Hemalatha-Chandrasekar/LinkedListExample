752b907fe8f06a5cf01aa783c9af41cfa1a2245e
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-11 14:21:56 -0600

    LinkedList Commit

--- src/LinkedListsortExample.java
+++ src/LinkedListsortExample.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class LinkedListsortExample {
+    public void LinkedListsortDemo() {
+        //Create linked list
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("C");
+        linkedList.add("B");
+        linkedList.add("D");
+    //Unsorted
+        System.out.println(linkedList);
+    //1. Sort the list
+        Collections.sort(linkedList);
+    //Sorted
+        System.out.println(linkedList);
+    //2. Custom sorting
+        Collections.sort(linkedList, Collections.reverseOrder());
+    //Custom sorted
+        System.out.println(linkedList);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+    LinkedListExample linkedListExample = new LinkedListExample();
+    linkedListExample.printList();
+    LinkedListsortExample linkedListExample2 = new LinkedListsortExample();
+    linkedListExample2.LinkedListsortDemo();
+    }
 }
